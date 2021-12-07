bd424c70e3bbc1056db7de2729402cc1c778753b
Andrii Shpek <ashpek01@gmail.com>
2021-12-08 01:59:47 +0200

    Cleanup code

--- admin/src/main/java/com/flaringapp/testingsimulator/admin/presentation/auth/login/AdminLoginViewBehaviour.kt
+++ admin/src/main/java/com/flaringapp/testingsimulator/admin/presentation/auth/login/AdminLoginViewBehaviour.kt
@@ -2,6 +2,8 @@ package com.flaringapp.testingsimulator.admin.presentation.auth.login
 
 import com.flaringapp.testingsimulator.presentation.features.auth.login.LoginViewBehaviour
 
-class AdminLoginViewBehaviour : LoginViewBehaviour{
+class AdminLoginViewBehaviour : LoginViewBehaviour {
+
     override val isSignUpEnabled: Boolean = false
+
 }
--- user/src/main/java/com/flaringapp/testingsimulator/user/presentation/auth/login/UserLoginViewBehaviour.kt
+++ user/src/main/java/com/flaringapp/testingsimulator/user/presentation/auth/login/UserLoginViewBehaviour.kt
@@ -2,6 +2,8 @@ package com.flaringapp.testingsimulator.user.presentation.auth.login
 
 import com.flaringapp.testingsimulator.presentation.features.auth.login.LoginViewBehaviour
 
-class UserLoginViewBehaviour : LoginViewBehaviour{
+class UserLoginViewBehaviour : LoginViewBehaviour {
+
     override val isSignUpEnabled: Boolean = true
+
 }
