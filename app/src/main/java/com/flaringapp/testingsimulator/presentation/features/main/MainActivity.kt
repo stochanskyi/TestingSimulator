908fe02631cba361fcacfb63b0d476948071695d
Andrii Shpek <ashpek01@gmail.com>
2021-12-08 14:53:57 +0200

    Fix nav graph initialization

--- app/src/main/java/com/flaringapp/testingsimulator/presentation/features/main/MainActivity.kt
+++ app/src/main/java/com/flaringapp/testingsimulator/presentation/features/main/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.flaringapp.testingsimulator.presentation.features.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import com.flaringapp.testingsimulator.R
 import com.flaringapp.testingsimulator.core.presentation.appbar.configuration.*
-import com.flaringapp.testingsimulator.presentation.R as PresentationR
 import com.flaringapp.testingsimulator.presentation.navigation.NavigationGraphProvider
 import org.koin.android.ext.android.inject
 
@@ -72,8 +71,11 @@ class MainActivity : AppCompatActivity(),
     }
 
     private fun initNavigationGraph() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) ?: return
+        val navController = navHost.findNavController()
+
         val graphId = navigationGraphProvider.provideGraphId()
-        findNavController(PresentationR.id.nav_host_fragment).setGraph(graphId)
+        navController.setGraph(graphId)
     }
 
     private fun updateAppBar(action: () -> Unit) {
