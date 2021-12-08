b1d6c19b50f7653f46439bd6c6585ff49f8f2e48
Andrii Shpek <ashpek01@gmail.com>
2021-12-08 18:52:33 +0200

    Rename emojis

--- presentation/src/main/java/com/flaringapp/testingsimulator/presentation/data/emoji/EmojiDefinitionImpl.kt
+++ presentation/src/main/java/com/flaringapp/testingsimulator/presentation/data/emoji/EmojiDefinitionImpl.kt
@@ -6,11 +6,11 @@ class EmojiDefinitionImpl : EmojiDefinition {
 
     //TODO fill with emojis
     private val emojisMap = mapOf(
-        1 to R.drawable.ic_emoji_testing_enabled
+        1 to R.drawable.img_emoji_testing_enabled
     )
 
     //TODO replace with real default emoji
-    private val defaultEmoji = R.drawable.ic_emoji_testing_enabled
+    private val defaultEmoji = R.drawable.img_emoji_testing_enabled
 
     override fun getEmojiById(emojiId: Int): Int? {
         return emojisMap[emojiId]
