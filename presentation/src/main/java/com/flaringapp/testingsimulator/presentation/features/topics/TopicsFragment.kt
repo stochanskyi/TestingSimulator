a916dee8408d6eb315bbb3c1e88b0ee16289c627
Marian Stochanskyi <stochan.andr@gmail.com>
2021-12-08 20:44:59 +0200

    Add profile action to topics toolbar

--- presentation/src/main/java/com/flaringapp/testingsimulator/presentation/features/topics/TopicsFragment.kt
+++ presentation/src/main/java/com/flaringapp/testingsimulator/presentation/features/topics/TopicsFragment.kt
@@ -1,8 +1,10 @@
 package com.flaringapp.testingsimulator.presentation.features.topics
 
+import android.view.MenuItem
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.flaringapp.testingsimulator.core.presentation.appbar.configuration.configureAppBarWithLifecycle
 import com.flaringapp.testingsimulator.presentation.R
 import com.flaringapp.testingsimulator.presentation.databinding.FragmentTopicsBinding
 import com.flaringapp.testingsimulator.presentation.features.topics.adapter.TopicsAdapter
@@ -12,7 +14,7 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class TopicsFragment : ModelledFragment(R.layout.fragment_topics) {
 
-    override val model : TopicsViewModel by viewModel()
+    override val model: TopicsViewModel by viewModel()
 
     private val binding by viewBinding { FragmentTopicsBinding.bind(it) }
 
@@ -21,6 +23,21 @@ class TopicsFragment : ModelledFragment(R.layout.fragment_topics) {
 
         topicsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         topicsRecyclerView.adapter = topicsAdapter
+
+        configureAppBarWithLifecycle {
+            menuId = R.menu.topics_menu
+            itemSelectedListener = this@TopicsFragment::onMenuItemSelected
+        }
+    }
+
+    private fun onMenuItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_profile -> {
+                model.openProfile()
+                true
+            }
+            else -> false
+        }
     }
 
     override fun observeModel() {
