e854c87d04e9c5a735a6c735d3ce76cbcefb8ff5
Andrii Shpek <ashpek01@gmail.com>
2021-12-08 18:48:05 +0200

    Implement emoji colors definition content, rename win emoji png

--- presentation/src/main/java/com/flaringapp/testingsimulator/presentation/data/emoji/EmojiColorsDefinitionImpl.kt
+++ presentation/src/main/java/com/flaringapp/testingsimulator/presentation/data/emoji/EmojiColorsDefinitionImpl.kt
@@ -5,12 +5,31 @@ import com.flaringapp.testingsimulator.presentation.R
 
 class EmojiColorsDefinitionImpl : EmojiColorsDefinition {
 
-    //TODO fill with emojis
     private val emojiColorsMap = mapOf(
         R.drawable.img_emoji_test to EmojiColorsRes(
             background = R.color.statistics_test_bg,
             accent = R.color.statistics_test_accent,
             variant = R.color.statistics_test_variant,
+        ),
+        R.drawable.img_emoji_student to EmojiColorsRes(
+            background = R.color.statistics_student_bg,
+            accent = R.color.statistics_student_accent,
+            variant = R.color.statistics_student_variant,
+        ),
+        R.drawable.img_emoji_eyes to EmojiColorsRes(
+            background = R.color.statistics_eyes_bg,
+            accent = R.color.statistics_eyes_accent,
+            variant = R.color.statistics_eyes_variant,
+        ),
+        R.drawable.img_emoji_brain to EmojiColorsRes(
+            background = R.color.statistics_brain_bg,
+            accent = R.color.statistics_brain_accent,
+            variant = R.color.statistics_brain_variant,
+        ),
+        R.drawable.img_emoji_win to EmojiColorsRes(
+            background = R.color.statistics_win_bg,
+            accent = R.color.statistics_win_accent,
+            variant = R.color.statistics_win_variant,
         ),
     )
 
