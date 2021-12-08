e64056ae126988795a22269e45e444830745e859
Andrii Shpek <ashpek01@gmail.com>
2021-12-08 18:30:35 +0200

    Add missing retrofit annotation

--- data/src/main/java/com/flaringapp/testingsimulator/data/network/features/profiles_statistics/ProfileStatisticsApi.kt
+++ data/src/main/java/com/flaringapp/testingsimulator/data/network/features/profiles_statistics/ProfileStatisticsApi.kt
@@ -2,9 +2,12 @@ package com.flaringapp.testingsimulator.data.network.features.profiles_statistic
 
 import com.flaringapp.testingsimulator.core.data.network.base.ApiResponseList
 import com.flaringapp.testingsimulator.data.network.features.profiles_statistics.response.ProfileStatisticsResponse
+import retrofit2.http.GET
 
 interface ProfileStatisticsApi {
 
+    // TODO API change route
+    @GET("")
     suspend fun getProfileStatistics(): ApiResponseList<ProfileStatisticsResponse>
 
 }
