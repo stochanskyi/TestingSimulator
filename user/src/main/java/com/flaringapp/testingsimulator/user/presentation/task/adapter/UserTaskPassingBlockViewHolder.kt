74000a09ab9169f8119cf2d58cdc9aeac6e1b6c6
Andrii Shpek <ashpek01@gmail.com>
2021-12-09 22:45:21 +0200

    Add elevation animation

--- user/src/main/java/com/flaringapp/testingsimulator/user/presentation/task/adapter/UserTaskPassingBlockViewHolder.kt
+++ user/src/main/java/com/flaringapp/testingsimulator/user/presentation/task/adapter/UserTaskPassingBlockViewHolder.kt
@@ -1,10 +1,13 @@
 package com.flaringapp.testingsimulator.user.presentation.task.adapter
 
+import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
 import androidx.recyclerview.widget.RecyclerView
+import com.flaringapp.testingsimulator.core.presentation.utils.dp
 import com.flaringapp.testingsimulator.core.presentation.utils.inflater
 import com.flaringapp.testingsimulator.user.databinding.ViewHolderUserTaskPassingBlockBinding
 import com.flaringapp.testingsimulator.user.presentation.task.models.UserTaskPassingBlockViewData
@@ -15,17 +18,24 @@ class UserTaskPassingBlockViewHolder private constructor(
 ): RecyclerView.ViewHolder(binding.root), UserTaskPassingBlockDraggableViewHolder {
 
     companion object {
+        private const val DRAG_ANIMATION_DURATION = 150L
+        private const val ELEVATION_DELTA = 4
+
         fun create(parent: ViewGroup) = UserTaskPassingBlockViewHolder(
             ViewHolderUserTaskPassingBlockBinding.inflate(parent.inflater, parent, false)
         )
     }
 
+    private var defaultElevation = 0f
+    private var elevationAnimator: ValueAnimator? = null
+
     override fun onDragStarted() {
-        // TODO animation
+        defaultElevation = itemView.elevation
+        itemView.animateElevation(defaultElevation + itemView.context.dp(ELEVATION_DELTA))
     }
 
     override fun onDragEnded() {
-        // TODO animation
+        itemView.animateElevation(defaultElevation)
     }
 
     fun bind(
@@ -51,10 +61,23 @@ class UserTaskPassingBlockViewHolder private constructor(
         layoutBlockEnabled.setOnClickListener(null)
         checkboxBlockEnabled.setOnCheckedChangeListener(null)
         buttonDrag.setOnTouchListener(null)
+
+        elevationAnimator?.cancel()
+        elevationAnimator = null
     }
 
     private fun beginDrag(dragListener: UserTaskPassingBlockTouchDragListener): Boolean {
         return dragListener.beginTouchDrag(this)
+    }
+
+    private fun View.animateElevation(to: Float) {
+        elevationAnimator?.cancel()
+        elevationAnimator = ValueAnimator.ofFloat(elevation, to).apply {
+            duration = DRAG_ANIMATION_DURATION
+            interpolator = DecelerateInterpolator()
+            addUpdateListener { elevation = it.animatedValue as Float }
+            start()
+        }
     }
 
     private inner class DragTouchListener(
