2952e7db5397d312ec102553bdc4e07c2ad771ea
Andrii Shpek <ashpek01@gmail.com>
2021-12-09 21:17:01 +0200

    Add skeleton for user task passing screen

---
+++ user/src/main/java/com/flaringapp/testingsimulator/user/presentation/task/UserTaskPassingFragment.kt
@@ -0,0 +1,16 @@
+package com.flaringapp.testingsimulator.user.presentation.task
+
+import com.flaringapp.testingsimulator.presentation.mvvm.ModelledFragment
+import com.flaringapp.testingsimulator.user.R
+import com.flaringapp.testingsimulator.user.databinding.FragmentUserTestDetailsBinding
+import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class UserTaskPassingFragment : ModelledFragment(R.layout.fragment_user_task_passing) {
+
+    override val model: UserTaskPassingModel by viewModel()
+
+    private val binding: FragmentUserTestDetailsBinding by viewBinding(
+        FragmentUserTestDetailsBinding::bind
+    )
+}
---
+++ user/src/main/java/com/flaringapp/testingsimulator/user/presentation/task/UserTaskPassingModel.kt
@@ -0,0 +1,7 @@
+package com.flaringapp.testingsimulator.user.presentation.task
+
+import com.flaringapp.testingsimulator.presentation.mvvm.BaseViewModel
+
+abstract class UserTaskPassingModel : BaseViewModel()
+
+class UserTaskPassingModelImpl: UserTaskPassingModel()
