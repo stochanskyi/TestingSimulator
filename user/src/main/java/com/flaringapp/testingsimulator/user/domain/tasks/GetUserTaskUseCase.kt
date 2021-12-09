4e49720569c6f268ced262d82872c1624f55dc7f
Andrii Shpek <ashpek01@gmail.com>
2021-12-09 22:47:49 +0200

    Mock get task use case

--- user/src/main/java/com/flaringapp/testingsimulator/user/domain/tasks/GetUserTaskUseCase.kt
+++ user/src/main/java/com/flaringapp/testingsimulator/user/domain/tasks/GetUserTaskUseCase.kt
@@ -3,13 +3,44 @@ package com.flaringapp.testingsimulator.user.domain.tasks
 import com.flaringapp.testingsimulator.core.data.common.call.CallResult
 import com.flaringapp.testingsimulator.user.data.repository.tasks.UserTasksRepository
 import com.flaringapp.testingsimulator.user.domain.tasks.model.UserTask
+import com.flaringapp.testingsimulator.user.domain.tasks.model.UserTaskBlock
 
 class GetUserTaskUseCase(
     private val tasksRepository: UserTasksRepository
 ) {
 
     suspend operator fun invoke(testId: Int): CallResult<UserTask> {
-        return tasksRepository.getTask(testId)
+//        return tasksRepository.getTask(testId)
+        
+        return CallResult.Success(
+            UserTask(
+                id = testId,
+                name = "Task $testId",
+                orderNumber = 1,
+                blocks = listOf(
+                    UserTaskBlock(
+                        id = 1,
+                        text = "Block 1"
+                    ),
+                    UserTaskBlock(
+                        id = 2,
+                        text = "Block 2"
+                    ),
+                    UserTaskBlock(
+                        id = 3,
+                        text = "Block 3"
+                    ),
+                    UserTaskBlock(
+                        id = 4,
+                        text = "Block 4"
+                    ),
+                    UserTaskBlock(
+                        id = 5,
+                        text = "Block 5 Block 5 Block 5 Block 5 Block 5 Block 5 Block 5 Block 5"
+                    )
+                )
+            )
+        )
     }
 
 }
