f3db6149211a42a239d6d7decdf3f149f4bddb21
Marian Stochanskyi <stochan.andr@gmail.com>
2021-12-10 02:19:20 +0200

    Fill emoji definitions with real data

--- presentation/src/main/java/com/flaringapp/testingsimulator/presentation/data/emoji/EmojiDefinitionImpl.kt
+++ presentation/src/main/java/com/flaringapp/testingsimulator/presentation/data/emoji/EmojiDefinitionImpl.kt
@@ -4,9 +4,20 @@ import com.flaringapp.testingsimulator.presentation.R
 
 class EmojiDefinitionImpl : EmojiDefinition {
 
-    //TODO fill with emojis
     private val emojisMap = mapOf(
-        1 to R.drawable.img_emoji_testing_enabled
+        1 to R.drawable.img_emoji_testing_enabled, // Tests enabled
+        2 to R.drawable.img_emoji_testing_disabled, // Tests disabled
+        3 to R.drawable.img_emoji_development_enabled, // Development enabled
+        4 to R.drawable.img_emoji_development_disabled, // Development disabled
+        5 to R.drawable.img_emoji_design_enabled, // Design enabled
+        6 to R.drawable.img_emoji_design_disabled, // Design disabled
+        7 to R.drawable.img_emoji_project_management_enabled, // PM enabled
+        8 to R.drawable.img_emoji_project_management_disabled, // PM disabled
+        9 to R.drawable.img_emoji_brain, // Passed tasks for user, Success rate for admin
+        10 to R.drawable.img_emoji_eyes, // Task tries for user, Attempts for admin
+        11 to R.drawable.img_emoji_note, // Tests taken for user, Tests published for admin
+        12 to R.drawable.img_emoji_student, // Passed tests for user, Students for admin
+        13 to R.drawable.img_emoji_win, // Average rate for user, - for admin
     )
 
     //TODO replace with real default emoji
