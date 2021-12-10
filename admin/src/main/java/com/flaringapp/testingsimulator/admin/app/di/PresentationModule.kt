31e50b772fe7226c316e68c27809522382a70e57
Marian Stochanskyi <stochan.andr@gmail.com>
2021-12-10 14:35:17 +0200

    Add initializing admin task view model

--- admin/src/main/java/com/flaringapp/testingsimulator/admin/app/di/PresentationModule.kt
+++ admin/src/main/java/com/flaringapp/testingsimulator/admin/app/di/PresentationModule.kt
@@ -46,6 +46,6 @@ val PresentationModule = module {
     viewModel<AdminTestDetailsViewModel> { AdminTestDetailsViewModeImpl(get(), get(), get(), get(), get()) }
     factory { AdminTestStatusIsEditableTransformer() }
 
-    viewModel<AdminTaskEditViewModel> { AdminTaskEditViewModelImpl() }
+    viewModel<AdminTaskEditViewModel> { AdminTaskEditViewModelImpl(get(), get()) }
 
 }
--- admin/src/main/java/com/flaringapp/testingsimulator/admin/presentation/task_edit/AdminTaskEditViewModel.kt
+++ admin/src/main/java/com/flaringapp/testingsimulator/admin/presentation/task_edit/AdminTaskEditViewModel.kt
@@ -1,11 +1,17 @@
 package com.flaringapp.testingsimulator.admin.presentation.task_edit
 
+import com.flaringapp.testingsimulator.admin.domain.tasks.CreateTaskUseCase
+import com.flaringapp.testingsimulator.admin.domain.tasks.EditTaskUseCase
 import com.flaringapp.testingsimulator.admin.domain.tasks.models.AdminTaskBlock
-import com.flaringapp.testingsimulator.admin.domain.tests.models.AdminTestTask
+import com.flaringapp.testingsimulator.admin.domain.tasks.models.AdminTaskDetailed
+import com.flaringapp.testingsimulator.core.app.common.clearAndAdd
 import com.flaringapp.testingsimulator.presentation.mvvm.BaseViewModel
 import kotlinx.coroutines.Job
+import java.lang.RuntimeException
 
 abstract class AdminTaskEditViewModel : BaseViewModel() {
+
+    abstract fun init(testId: Int, taskId: Int?)
 
     abstract fun setBlockText(id: Int, text: String)
 
@@ -21,18 +27,30 @@ abstract class AdminTaskEditViewModel : BaseViewModel() {
 
 }
 
-class AdminTaskEditViewModelImpl : AdminTaskEditViewModel() {
+class AdminTaskEditViewModelImpl(
+    private val createTaskUseCase: CreateTaskUseCase,
+    private val editTaskUseCase: EditTaskUseCase
+) : AdminTaskEditViewModel() {
 
     private var testId: Int? = null
     private var taskId: Int? = null
 
-    private var currentTask: AdminTestTask? = null
+    private var currentTask: AdminTaskDetailed? = null
 
     private val orderedBlocks: MutableList<AdminTaskBlock> = mutableListOf()
     private val disabledBlocks: MutableSet<Int> = hashSetOf()
     private val linkedBlock: MutableMap<Int, Int> = mutableMapOf()
 
     private var proceedJob: Job? = null
+
+    override fun init(testId: Int, taskId: Int?) {
+        this.testId = testId
+        this.taskId = taskId
+
+        if (taskId != null) {
+            loadTask(taskId)
+        }
+    }
 
     override fun setBlockText(id: Int, text: String) {
         // TODO impl
@@ -57,4 +75,26 @@ class AdminTaskEditViewModelImpl : AdminTaskEditViewModel() {
     override fun saveChanges() {
         // TODO impl
     }
+
+    private fun loadTask(taskId: Int) {
+        //TODO load task
+        val task = taskMockFunc()
+
+        currentTask = task
+        orderedBlocks.clearAndAdd(task.blocks)
+        disabledBlocks.clearAndAdd(getDisabledBlocks(task))
+    }
+
+    private fun getDisabledBlocks(task: AdminTaskDetailed): Set<Int> {
+        return task.blocks
+            .asSequence()
+            .filter { !it.isEnabled }
+            .map { it.id }
+            .toSet()
+    }
+
+    private fun taskMockFunc(): AdminTaskDetailed {
+        throw RuntimeException()
+    }
+
 }
