509627f49fa3bf33f7bab44ad963b3bab571eab7
Andrii Shpek <ashpek01@gmail.com>
2021-12-10 02:48:26 +0200

    Complete admin login api

--- admin/src/main/java/com/flaringapp/testingsimulator/admin/data/network/features/auth/AdminAuthApi.kt
+++ admin/src/main/java/com/flaringapp/testingsimulator/admin/data/network/features/auth/AdminAuthApi.kt
@@ -9,7 +9,7 @@ import retrofit2.http.POST
 interface AdminAuthApi {
 
     //TODO change route
-    @POST("login")
+    @POST("User/login")
     suspend fun login(
         @Body request: AdminLoginRequest
     ): ApiResponse<AdminLoginResponse>
--- admin/src/main/java/com/flaringapp/testingsimulator/admin/domain/login/AdminLoginUseCase.kt
+++ admin/src/main/java/com/flaringapp/testingsimulator/admin/domain/login/AdminLoginUseCase.kt
@@ -1,11 +1,10 @@
 package com.flaringapp.testingsimulator.admin.domain.login
 
+import com.flaringapp.testingsimulator.admin.data.repository.AdminDataRepository
 import com.flaringapp.testingsimulator.admin.data.repository.auth.AdminAuthRepository
+import com.flaringapp.testingsimulator.admin.data.repository.profile.AdminProfileRepository
 import com.flaringapp.testingsimulator.core.data.common.call.CallResultNothing
 import com.flaringapp.testingsimulator.domain.features.auth.LoginUseCase
-import com.flaringapp.testingsimulator.admin.data.repository.AdminDataRepository
-import com.flaringapp.testingsimulator.admin.data.repository.profile.AdminProfileRepository
-import com.flaringapp.testingsimulator.core.data.common.call.CallResult
 
 class AdminLoginUseCase(
     private val authRepository: AdminAuthRepository,
@@ -18,10 +17,6 @@ class AdminLoginUseCase(
         password: String,
         rememberMe: Boolean
     ): CallResultNothing {
-        //TODO remember me
-
-        return CallResult.Success(Unit)
-
         return authRepository.login(
             email = email,
             password = password
